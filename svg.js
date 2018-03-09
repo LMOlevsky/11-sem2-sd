2a01b23ff79c20feab61eb2650c5438a888c941f
LMOlevsky <lolevsky@stuy.edu>
2018-03-09 14:04:55 -0500

    staerted

--- svg.js
+++ svg.js
@@ -1,18 +1,10 @@
 var clear = document.getElementById("clear");
 var stop = document.getElementById("stop");
-var circle = document.getElementById("circle");
-var dvd = document.getElementById("dvd");
 var svg = document.getElementById("svg");
 
 var id;
 
-//for circle
-var size, grow;
-//size = 1;
-//grow = 1;
-
 //for dvd
-var diffX, diffY;
 
 var clearCallback = function(e){
     svg.innerHTML = "";
@@ -22,42 +14,14 @@ var stopCallback = function(e){
     clearInterval(id);
 };
 
-var circleCallback = function(e){
-    size = 1;
-    grow = 1;
-    stopCallback();
-    
-    var run = function(){
-	clearCallback();
-	var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-	c.setAttribute("cx", parseInt(svg.getAttribute("width"))/2);
-	c.setAttribute("cy", parseInt(svg.getAttribute("height"))/2);
-	c.setAttribute("r", size);
-	svg.appendChild(c);
-	if (size == (parseInt(svg.getAttribute("width"))+parseInt(svg.getAttribute("height")))/4){
-	    grow = 0;
-	}
-	else if (size == 0){
-	    grow = 1;
-	}
-	if (grow == 1){
-	    size++;
-	}
-	else{
-	    size--;
-	}
-    };
-    id = setInterval(run, 10);
-};
 
 var dvdCallback = function(e){
-    var x = parseInt(svg.getAttribute("width"))/2
-    var y = parseInt(svg.getAttribute("height"))/2;
+    var x = e.offsetX;
+    var y = e.offsetY;
+    var diffX, diffY;
     diffX = 2;
     diffY = 1;
-    
-    stopCallback();
-    
+
     var run = function(){
 	clearCallback();
 	var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
@@ -85,5 +49,4 @@ var dvdCallback = function(e){
 
 clear.addEventListener("click", clearCallback);
 stop.addEventListener("click", stopCallback);
-circle.addEventListener("click", circleCallback);
-dvd.addEventListener("click", dvdCallback);
+svg.addEventListener("click", dvdCallback);
